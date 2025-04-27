512bae30e68f85b5885b43baaf8780491139f09a
Xavi Flores <apptivas@Xavis-Mac-mini.local>
2025-04-27 09:11:54 -0600

    create stream client

--- database/postgres.go
+++ database/postgres.go
@@ -51,3 +51,41 @@ func (repo *PostgresRepository) GetStudent(ctx context.Context, id string) (*mod
 	}
 	return &student, nil
 }
+
+func (repo *PostgresRepository) SetTest(ctx context.Context, test *models.Test) error {
+	_, err := repo.db.ExecContext(ctx, "INSERT INTO tests (id, name) VALUES ($1, $2)", test.Id, test.Name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *PostgresRepository) GetTest(ctx context.Context, id string) (*models.Test, error) {
+	rows, err := repo.db.QueryContext(ctx, "SELECT id, name FROM tests WHERE id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	var test = models.Test{}
+	for rows.Next() {
+		err := rows.Scan(&test.Id, &test.Name)
+		if err != nil {
+			return nil, err
+		}
+		return &test, nil
+	}
+	return &test, nil
+}
+
+func (repo *PostgresRepository) SetQuestion(ctx context.Context, question *models.Question) error {
+	_, err := repo.db.ExecContext(ctx, "INSERT INTO questions (id, answer,question,test_id) VALUES ($1, $2, $3, $4)", question.Id, question.Answer, question.Question, question.TestId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
