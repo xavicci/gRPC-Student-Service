861620fa72956ed4cedb33a815b9f7b3d0d884e1
Xavi Flores <xa_franc@live.com>
2025-04-29 22:33:07 -0600

    client grpc

---
+++ client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/xavicci/gRPC-Student-Service/testpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	cc, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+	c := testpb.NewTestServiceClient(cc)
+	DoUnary(c)
+}
+
+func DoUnary(c testpb.TestServiceClient) {
+	req := &testpb.GetTestRequest{
+		Id: "t1",
+	}
+
+	res, err := c.GetTest(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling GetTest: %v", err)
+	}
+	log.Printf("response from GetTest: %v", res)
+}
